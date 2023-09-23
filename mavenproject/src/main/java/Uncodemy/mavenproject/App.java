59c16754b601cda72ff269eb14dd5cee0bebbb1a
91637 <91637@ROHIT>
2023-09-24 01:25:51 +0530

    This is my project

---
+++ mavenproject/src/main/java/Uncodemy/mavenproject/App.java
@@ -0,0 +1,13 @@
+package Uncodemy.mavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mavenproject/src/test/java/Uncodemy/mavenproject/mavenprojectclass.java
@@ -0,0 +1,12 @@
+package Uncodemy.mavenproject;
+
+import org.testng.annotations.Test;
+
+public class mavenprojectclass {
+	
+	@Test
+	public void testmethod() {
+		
+	}
+
+}
